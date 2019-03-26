cd82b2249ab3d642353adbed3a46591d05ba2602
Ruofan He <hrinternship2018@gmail.com>
2019-03-27 00:01:35 +0900

    Change cameraException

--- src/trackModule/cameraException.hpp
+++ src/trackModule/cameraException.hpp
@@ -1,10 +1,14 @@
 #pragma once
 #include <stdexcept>
+#include <string>
+
+using std::string;
 
 //Exception class about camera
 class CameraException: public std::runtime_error{
     public:
     CameraException(const char* message):runtime_error(message){}
-    CameraException():runtime_error("CameraException"){}
+    CameraException(const string& message):runtime_error(message){}
+    CameraException():runtime_error("Failed something about camera"){}
     char* what();//return message
 };
